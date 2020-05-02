18eba471367a85c33f6902762de57f1d30e4085e
Caleb Moses <caleb@dragonfly.co.nz>
2020-05-03 00:35:57 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt', 'r') as f:
+    reqs = f.read().split()[:-2]
+
 setup(
     name='reo_toolkit',
     version='0.1',
@@ -7,7 +10,7 @@ setup(
     license='Kaitiakitanga License',
     description='A python package for manipulating māori language text',
     long_description=open('README.md').read(),
-    install_requires=['pytest'],
+    install_requires=reqs,
     url='https://github.com/TeHikuMedia/reo-toolkit',
     author='Caleb Moses',
     author_email='caleb@dragonfly.co.nz'
