f9c5818454855051ac37893d373ea61e35deb809
Caleb Moses <caleb@dragonfly.co.nz>
2020-05-05 03:59:12 +1200

    Update is_maori

--- reo_toolkit/reo_toolkit.py
+++ reo_toolkit/reo_toolkit.py
@@ -40,16 +40,26 @@ def is_maori(text, drop_ambiguous = False):
     if is_camel_case(text):
         return all(is_maori(sub.lower()) for sub in camel_case_split(text))
 
+    raw_text = text
+    text = BaseEncoder().encode(text)
 
     # Remove non alphabet characters
     text = re.sub(r"[^{}0-9\-\s]".format(''.join(consonants.union(vowels))), "",
                   text).strip()
 
+    non_maori_chars = set(ch for ch in text.lower() if ch in 'bcdfgjlqsvxyz')
+    if len(non_maori_chars) > 0:
+        logging.debug("Text contains non-maori letters: {}".format(
+            ', '.join(non_maori_chars)))
         return False
 
     if len(text) == 0:
-        # String is empty
-        logging.debug("String is empty after cleaning")
+        if re.search('[A-z]', raw_text):
+            # String is empty
+            logging.debug("String {} is empty after cleaning".format(raw_text))
+            return False
+        else:
+            return True
 
     if text in non_maori:
         logging.debug("Text {} is in non_maori word list".format(text))
@@ -60,16 +70,14 @@ def is_maori(text, drop_ambiguous = False):
         return False
 
     if len(text) == 1:
-        if text in vowels:
-            return True
-        else:
-            logging.debug("Single character word {} is not a vowel".format(text))
+        if text in consonants:
+            logging.debug("Single character word {} is a consonant".format(text))
             return False
-
-    if "-" in text:
-        return all(is_maori(sub) for sub in text.split("-"))
+        else:
+            return True
 
     if _triple_vowels.search(text):
+        logging.debug("Text {} contains triple vowel".format(text))
         return False
 
     for current_ch, next_ch in pairwise(text):
@@ -89,13 +97,13 @@ def is_maori(text, drop_ambiguous = False):
                                   .format(current_ch, next_ch, text))
                 return False
 
-    if next_ch in consonants:
+    if text[-1] in consonants:
         # Last character in word is a consonant
         logging.debug("The last character '{}' is a consonant: {}".format(
                 next_ch, text))
         return False
 
-    if next_ch == "-" and not current_ch in vowels:
+    if text[-1] == "-" and not current_ch in vowels:
         logging.debug("The next character is {} but the current character {} is not a vowel"\
                   .format(next_ch, current_ch))
         return False
