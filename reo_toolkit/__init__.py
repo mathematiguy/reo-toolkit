e559b17b985e60fc4d3d4b7ecbdcf36f1152fbb3
caleb.moses <calebjdmoses@gmail.com>
2025-05-08 16:06:05 -0400

    Import count_maori_violations

--- reo_toolkit/__init__.py
+++ reo_toolkit/__init__.py
@@ -1,4 +1,4 @@
 import re
 
-from .reo_toolkit import is_maori, ambiguous, vowels, consonants
+from .reo_toolkit import is_maori, ambiguous, vowels, consonants, count_maori_violations
 from . import encoders, numbers
